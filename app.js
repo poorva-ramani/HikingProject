76d066347fb964824abaeed370549f5031410b21
poorva <poorva.ramani@gmail.com>
2019-10-26 13:56:27 -0500

    some changes

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ $(document).ready(function () {
         address = searchInput.val();
         $(".searchBox").hide();
         $(".results").show();
+
         var geoCodeApi = {
             "async": true,
             "crossDomain": true,
@@ -32,22 +33,62 @@ $(document).ready(function () {
             }
             $.ajax(settings).done(function (result) {
                 console.log(result);
-                $('.myCard').clone().appendTo(".results");
                 var trails = result.trails;
-                for (var i = 0; i < trails.length; i++) {
-                    // $(".card-content").eq(i).text();
-                    var resultsDiv = $(".card-content").eq(i);
-                    var rating = $("<p>").text("Star rating: " + trails[i].starVotes);
-                    var difficulty = $("<p>").text("difficulty: " + trails[i].difficulty);
-                    resultsDiv.append(image);
-                    resultsDiv.append(rating);
-                    resultsDiv.append(difficulty);
-                    // $(".results").append(resultsDiv);
+                // var resultsDiv = $(".card-content").eq(0);
+                //     var rating = $("<p>").text("Star rating: " + trails[0].starVotes);
+                //     var difficulty = $("<p>").text("difficulty: " + trails[0].difficulty);
+                //     resultsDiv.append(image);
+                //     resultsDiv.append(rating);
+                //     resultsDiv.append(difficulty);
+                //     // $(".results").append(resultsDiv);
+                //     var image = $("<img>");
+                //     console.log(trails[0].imgMedium);
+                //     image.attr("src", trails[0].imgMedium);
+                 //   $(".card-image").eq(0).append(image);
+                for (var i = 1; i < trails.length; i++) { 
+                    $('.myCard').clone().appendTo(".results");  
                     var image = $("<img>");
                     image.attr("src", trails[i].imgMedium);
                     $(".card-image").eq(i).append(image);
+
+                    var resultsDiv = $(".card-content").eq(i);
+                    var rating = $("<p>").text("Star rating: " + trails[i].starVotes);
+                    var difficulty = $("<p>").text("difficulty: " + trails[i].difficulty);
+                    resultsDiv.append(rating);
+                    resultsDiv.append(difficulty);
+                   
                 }
             });
         });
     });
+
+    //this click function only to test if statements!!!
+   $("#filterSearchBtn").on('click', function(){
+       ratingFilter()
+   })
+   //var ratings = result.trails.stars
+   function ratingFilter() {
+   if (!$('#rating5').is(':checked') && !$('#rating4').is(':checked') && !$('#rating3').is(':checked')
+    && !$('#rating2').is(':checked') && !$('#rating1').is(':checked')) {
+       $('#errorMsg').html('Please have at least one option checked')
+       return;
+   }
+   if ($('#rating5').is(':checked')) {
+      // minStars == 5
+       console.log ('min5 checked')
+   }
+   if ($('#rating4').is(':checked')) {
+      // minStars == 4
+       console.log('min4 checked')
+   }
+   if ($('#rating3').is(':checked')) {
+      // minStars == 3
+   }
+   if ($('#rating2').is(':checked')) {
+      // minStars == 2
+   }
+   if ($('#rating1').is(':checked')) {
+      // minStars == 1
+   }
+   }
 });
