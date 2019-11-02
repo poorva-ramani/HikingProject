3c98f8f8605d0769ae67916b0c9e4581576b658b
poorva <poorva.ramani@gmail.com>
2019-11-01 23:15:48 -0500

    updating side nav and history stuff

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,16 +3,14 @@ var closeBtn = document.querySelector("#close-btn");
 var closeButton = document.querySelector(".close-button");
 var modalText = document.querySelector("#modal-text");
 var modalSubtext = document.querySelector("#modal-subtext");
-var i=0;
-
+var mymap= L.map('mapid').setView([43, -83], 12);
+var i = 0;
 $(document).ready(function () {
     var searchInput = $('#searchInp');
     var searchButton = $('#searchBtn');
     var trailLength = '';
     var stars = '1';
     var APIKEY = '200624582-ef03dfcbf90f2bd9243bdef3d1acb99b';
-
-
     $(".results").hide();
     $("#filterSearchBtn").on('click', function () {
         // ratingFilter()
@@ -42,10 +40,9 @@ $(document).ready(function () {
     }
 
     //geolocation and hiking api    
-    function searchResults(trailLength, stars) {
+    function searchResults(address, trailLength, stars) {
         $('.searchResults').empty()
-        address = searchInput.val();
-        //code for popupmodal for blank searches
+        console.log(address)
         if (address === '') {
             $('.searchResults').on("click", toggleModal());
             modalText.textContent = " Blank Search !!!";
@@ -75,8 +72,25 @@ $(document).ready(function () {
                 "url": queryURL,
                 "method": "GET",
             }
-            var mymap = L.map('mapid').setView([LATITUDE, LONGITUDE], 12)
-
+            
+             //setting map view point
+            //  $.empty(mymap)
+            mymap.invalidateSize();
+            mymap = L.map('mapid').setView([LATITUDE, LONGITUDE], 12)
+             
+             //map
+             L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+                 attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+                 maxZoom: 18,
+                 id: 'mapbox.streets',
+                 accessToken: 'pk.eyJ1IjoiZGVicmFzcGFyciIsImEiOiJjazJiNmJ2cDUwMHg5M2NxY29yeGQ0cWowIn0._VZcZvPTCyIjGPjjz3FG7w'
+             }).addTo(mymap);
+             var circle = L.circle([LATITUDE, LONGITUDE], {
+                 color: 'red',
+                 fillColor: '#f03',
+                 fillOpacity: 0.5,
+                 radius: 150
+             }).addTo(mymap);
             $.ajax(settings).done(function (result) {
                 var trails = result.trails;
                  //if no results are rendered - would form into a popup... Unless Poorva already did that
@@ -172,11 +186,54 @@ $(document).ready(function () {
         });
     }
 
+    //localstorage methods
+    var searchedCities = JSON.parse(localStorage.getItem('searchedCities')) || []
+
+    function appendSearches() {
+        $("#history").empty();
+        for (i = 0; i < searchedCities.length; i++) {
+            var li = $("<li>")
+            li.addClass("center")
+            li.attr("data-city", searchedCities[i].name)
+            $("#history").prepend(li)
+            li.click(function (e) {
+                recall(e.target.innerText)
+            });
+            li.append(searchedCities[i].name);
+        }
+    }
+
+    function recall(address) {
+        console.log(1)
+        closeNav();
+        searchResults(address);
+    }
 
     //code to trigget search action
     searchButton.click(function (e) {
         e.preventDefault();
-        searchResults();
+        $('.searchResults').empty()
+        address = searchInput.val();
+        //code for popupmodal for blank searches
+        searchResults(address);
+        //storing in localstorage
+        var cityObj = {
+            name: address
+        }
+        searchedCities.push(cityObj)
+        localStorage.setItem('searchedCities', JSON.stringify(searchedCities))
+        if (searchedCities.length === 10) {
+            searchedCities.shift();
+        }
+        appendSearches()
     });
 
 });
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
